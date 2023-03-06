dab838a62fa8f32bbbe3a6e5bfe2c1d637186b99
A D SUMMETH RAJU <summethsusubros@gmail.com>
2023-03-06 23:52:45 +0530

    callback url changed

--- versionassist/auth.js
+++ versionassist/auth.js
@@ -8,8 +8,9 @@ const GOOGLE_CLIENT_SECRET = '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';
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  /* callbackURL: "http://v-a.onrender.com:3000/auth/google/callback",*/ 
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: "http://version-assist.mt.vcnash.oraclevcn.com",
+  /* callbackURL: "http://localhost:3000/auth/google/callback", */
+  
 },
 function(accessToken, refreshToken, profile, done) {
   
